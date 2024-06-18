b88370cee7566b86a639f2a73e64b204b705f03c
deshanii <deshanithilakshana2004@gmail.com>
2024-06-19 00:42:12 +0530

    first comnmit

---
+++ app.js
@@ -0,0 +1,78 @@
+
+// console.log(document.getElementById("username"));
+// console.log(document.getElementById("password"));
+// console.log(document.getElementById("age"));
+// console.log(document.getElementById("btn_login_onAction"));
+
+// document.getElementById("btn_login_onAction").addEventListener("click", loginOnAction);
+
+// function loginOnAction() {
+//     let userName = document.getElementById("username").value;
+//     let userPassword = document.getElementById("password").value;
+//     let userAge = document.getElementById("age").value;
+
+
+    //---------------------------------------------
+
+//     if(userAge>=18){
+//         console.log("you're eligible");
+//         alert("you're eligible")
+//     }else{
+//         console.log("you're not eligible :(");
+//         alert("you're not eligible :(")
+//     }
+
+//     switch (userAge) {
+//         case "18": alert("your age is 18");
+//             break;
+//         case "19": alert("your age is 19");
+//             break;
+
+//     }
+
+
+//     console.log(userName)
+//     console.log(userPassword)
+//     console.log(userAge)
+// }
+
+
+    // let numbers=[10,20,30,40,50,60,70,80,90];
+
+    // for(let i=0; i<numbers.length; i++){
+    //     console.log(numbers[i]);
+    // }
+
+    // numbers.push(100);
+
+    // console.log(numbers);
+
+
+
+    const customers=[
+        {
+            name:"saman",
+            age:10,
+            address:"gampaha"
+        },
+
+        {
+            name:"kamal",
+            age:10,
+            address:"gampaha"
+        },
+
+        {
+            name:"nimal",
+            age:10,
+            address:"gampaha"
+        },
+
+        {
+            name:"sunil",
+            age:10,
+            address:"gampaha"
+        }
+    ]
+
+    console.log(customers[1].name);
